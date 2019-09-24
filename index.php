a321cfa9e605bcab5c8e30ede09a393da53de1ea
refan8881 <refan7151@gmail.com>
2019-09-24 11:53:28 +0700

    bootstrap

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>latihan botstrap</title>
+    
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css"/>
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-md-12" style="background:violet">
+<h1>12 column</h1></div>
+
+
+</div>
+</div>
+<div class="container">       
+<div class="row">
+<div class="col-md-6" style="background:red">
+<h1>6 column</h1></div>
+
+
+
+<div class="col-md-6" style="background:blue">
+<h1>6 column</h1></div>
+
+</div>
+</div>
+
+
+
+<script src="/asset/css/bootstrap.bundle.js"></script>
+<script src="/asset/css/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
